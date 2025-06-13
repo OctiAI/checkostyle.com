5210ef4560e38f290e8f9d3e9500feed08b2bc57
OctiAI <project.thirdiris@gmail.com>
2025-06-13 17:17:59 -0400

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero: React.FC = () => {
             transition={{ duration: 0.6, delay: 0.2 }}
           >
             Your Stripe Checkout
-            <br /> Might Be 
+            <br /> Might Be  
             <span className="text-5xl md:text-7xl" style={{ color: '#E63946' }}>
              Killing
             </span>

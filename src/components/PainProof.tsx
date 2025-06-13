ecb49e18c673cf44b8a7f0b6eae5d4c0f35a2215
OctiAI <project.thirdiris@gmail.com>
2025-06-13 17:01:55 -0400

    Updated PainProof.tsx

--- src/components/PainProof.tsx
+++ src/components/PainProof.tsx
@@ -32,7 +32,7 @@ const PainProof: React.FC = () => {
           {/* Default Stripe - Pain Point */}
           <div className="relative">
             <div className="absolute -top-4 -left-4 bg-red-500 text-white px-3 py-1 rounded-full text-sm font-semibold flex items-center space-x-1">
-              <AlertTriangle className="h-4 w-2" />
+              <AlertTriangle className="h-4 w-8" />
               <span>Conversion Killer</span>
             </div>
             <div className="bg-gray-50 rounded-2xl p-8 border-2 border-red-200">

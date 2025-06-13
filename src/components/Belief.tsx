0dfd5a83302b5c69bccdd31107ee7976b59121d5
OctiAI <project.thirdiris@gmail.com>
2025-06-12 21:04:11 -0400

    Updated Belief.tsx

--- src/components/Belief.tsx
+++ src/components/Belief.tsx
@@ -40,7 +40,7 @@ const Belief: React.FC = () => {
           </h2>
         </motion.div>
 
-        <div className="grid lg:grid-cols-2 gap-12 items-center mx-auto">
+        <div className="grid lg:grid-cols-2 gap-12 items-center ml-12">
           {/* Benefits List */}
           <motion.div
             className="space-y-8"

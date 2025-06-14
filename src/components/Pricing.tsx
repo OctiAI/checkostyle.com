16b87a1f43226f03753f941a84ea9ad035edf93a
OctiAI <project.thirdiris@gmail.com>
2025-06-14 10:02:38 -0400

    Updated Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -93,7 +93,7 @@ const Pricing: React.FC = () => {
         return {
           price: '$948',
           period: '/year',
-          description: 'DFY comes free (save 500$)',
+          description: 'DFY free (saves $500)',
         };
       default:
         return {

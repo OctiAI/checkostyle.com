d406512c93b1131b544e7b94fe4899737d6874c4
OctiAI <project.thirdiris@gmail.com>
2025-06-16 15:07:07 -0400

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,10 +30,10 @@ const Hero: React.FC = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.2 }}
           >
-             <span className="text-5xl md:text-7xl" style={{ color: '#E63946' }}>Your </span> Stripe Checkout
+             <span style={{ color: '#E63946' }}>Your </span> Stripe Checkout
             <br />Might Be&nbsp;
 
-            <span className="text-5xl md:text-7xl" style={{ color: '#E63946' }}>
+            <span style={{ color: '#E63946' }}>
               Killing
             </span>
             <br />

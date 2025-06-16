69eec9015e12edb81c98848a0463c7f91f52752b
OctiAI <project.thirdiris@gmail.com>
2025-06-16 16:10:02 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid md:grid-cols-4 gap-8">
           {/* Logo & Description */}
-          <div className="md:col-span-2">
+          <div className="md:col-span-3">
             <motion.div 
               className="flex items-center space-x-2 mb-4"
               whileHover={{ scale: 1.05 }}

59f14ad4c9e7fc34bb203c888836e6e10b4ec7de
OctiAI <project.thirdiris@gmail.com>
2025-06-16 15:07:54 -0400

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero: React.FC = () => {
              <span style={{ color: '#E63946' }}>Your </span> Stripe Checkout
             <br />Might Be&nbsp;
 
-            <span style={{ color: '#E63946' }}>
+            <span className="text-red-600"}>
               Killing
             </span>
             <br />

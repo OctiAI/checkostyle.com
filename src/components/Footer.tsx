f2f6589f7f28f2990bce526540b36de0af5ee6a5
OctiAI <project.thirdiris@gmail.com>
2025-06-16 16:13:13 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-900 text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid md:grid-cols-8 gap-8">
+        <div className="grid md:grid-cols-2 gap-8">
           {/* Logo & Description */}
           <div className="md:col-span-2">
             <motion.div 

267f703a7450a2db1339fc981a143bbc1872c84e
OctiAI <project.thirdiris@gmail.com>
2025-06-17 12:13:34 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './form.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', 'ThankYouCall.html', 'ThankYouPage.html', './form.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },

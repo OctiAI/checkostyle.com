528251062dd1de4bef54b1f3c15391567e866c7a
OctiAI <project.thirdiris@gmail.com>
2025-06-17 18:01:05 -0400

    Updated Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -29,7 +29,7 @@ const FormPage = () => {
           <div>
             <div className="bg-gray-50 rounded-2xl" style={{ minHeight: '432px' }}>
                 <iframe
-                  src="./form"
+                  src="./Footer"
                   className="w-full h-[600px] border-none rounded-2xl shadow-lg"
                   title="Book My Free Call"
                 />

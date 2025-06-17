1c1ed0e20b149c8091b67434563a5226b4284d36
OctiAI <project.thirdiris@gmail.com>
2025-06-17 11:33:00 -0400

    Updated form.tsx

--- src/form.tsx
+++ src/form.tsx
@@ -4,15 +4,12 @@ import './index.css';
 
 import React from 'react';
 import Top from './components/Top';
-import Form from './components/Form';
-import Pitch from './components/Pitch';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
         <div className="min-h-screen">
           <Top/>
-          <Form/>
-          <Pitch/>
         </div>
   </StrictMode>
 );

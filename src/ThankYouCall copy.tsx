b4143b5d31c440435974afd9ede778f6481c3b07
OctiAI <project.thirdiris@gmail.com>
2025-06-17 12:18:49 -0400

    Added ThankYouCall copy.tsx

---
+++ src/ThankYouCall copy.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import React from 'react';
+import Form from './components/ThankYouCall.tsx';
+
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <Form/>
+    </StrictMode>
+);

589ebb12ec8ddcf26b65d3c166adb65f875e6047
OctiAI <project.thirdiris@gmail.com>
2025-06-17 17:30:46 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         main:    resolve(__dirname, 'index.html'),
         thankyou: resolve(__dirname, 'thankyoupage.html'),
         thankyouCall: resolve(__dirname, 'thankyoucall.html'),
+        bookacall: resolve(__dirname, 'bookacall.html'),        
       }
     }
   }

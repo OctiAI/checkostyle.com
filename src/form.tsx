1d07f04371ac45f8d26ffe062c59360fbf0fd71f
OctiAI <project.thirdiris@gmail.com>
2025-06-17 11:56:32 -0400

    Updated form.tsx

--- src/form.tsx
+++ src/form.tsx
@@ -6,6 +6,21 @@ import React from 'react';
 import Form from './components/Form';
 
 
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import ThankYouCall from './ThankYouCall';
+import ThankYouPayment from './ThankYouPayment';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThankYouCall />
+  </StrictMode>
+);
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
         <div className="min-h-screen">

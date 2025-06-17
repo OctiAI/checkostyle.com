c59d6f8fd10d2bdfb998c6597d006eb19ea1f561
OctiAI <project.thirdiris@gmail.com>
2025-06-17 15:59:01 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  build: {
+    rollupOptions: {
+      input: {
+        main:    resolve(__dirname, 'index.html'),
+        thankyou: resolve(__dirname, 'ThankYouPage.html'),
+        thankyouCall: resolve(__dirname, 'ThankYouCall.html'),
+      }
+    }
+  }
 });

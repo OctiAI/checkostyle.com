63d0d444a629a0a518a9f6306b19da18ecfebf74
OctiAI <project.thirdiris@gmail.com>
2025-06-17 11:33:36 -0400

    Updated form.tsx

--- src/form.tsx
+++ src/form.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import React from 'react';
-import Form from './components/Top';
+import Form from './components/Form';
 
 
 createRoot(document.getElementById('root')!).render(

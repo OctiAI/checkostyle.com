ffbfcada57ec76836a3febd6f63fc31f52a37e05
OctiAI <project.thirdiris@gmail.com>
2025-06-17 12:16:42 -0400

    Updated ThankYouCall.tsx

--- src/ThankYouCall.tsx
+++ src/ThankYouCall.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import React from 'react';
-import Form from './components/Form';
+import Form from './components/ThankYouCall.tsx';
 
 
 createRoot(document.getElementById('root')!).render(

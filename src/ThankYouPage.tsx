16cd3f5f9a449bfb31fb1fc84e2ce8e2ec27e160
OctiAI <project.thirdiris@gmail.com>
2025-06-17 12:20:27 -0400

    Updated ThankYouPage.tsx

--- src/ThankYouPage.tsx
+++ src/ThankYouPage.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import React from 'react';
-import Form from './components/ThankYouPage.tsx';
+import ThankYouPage from './components/ThankYouPage.tsx';
 
 
 createRoot(document.getElementById('root')!).render(

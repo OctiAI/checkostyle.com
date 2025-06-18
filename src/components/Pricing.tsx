79121e7c1f90a6bb61268d0a7abeb23301c6e184
OctiAI <project.thirdiris@gmail.com>
2025-06-18 14:15:50 -0400

    Updated Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -83,7 +83,7 @@ const Pricing: React.FC = () => {
     }
     switch (plan.name) {
       case 'Pro':
-        return { price: '$79', period: '/month', description: 'Billed at $948/year' , plan_ID: 'CS_checko_DFY_Y' };
+        return { price: '$79', period: '/month', description: 'Billed at $948/year' , plan_ID: 'CS_checko_Pro_Y' };
       case 'DFY':
         return { price: '$79', period: '/month ', description: ' $948/year (Best Value)', plan_ID: 'CS_checko_DFY_Y'};
       default:

6ba3ef47dcffcaccab728ff0b05ddc2572478bd2
OctiAI <project.thirdiris@gmail.com>
2025-06-18 20:10:06 -0400

    Updated checkoutLinker.tsx

--- src/checkoutLinker.tsx
+++ src/checkoutLinker.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import CheckoutPage from './components/checkout';
 
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+
 function App() {
   return <CheckoutPage />;
 }

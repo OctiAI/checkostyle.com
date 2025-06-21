b5b242b5041bd8ed4cd08cdc3a5b2005a8510993
OctiAI <project.thirdiris@gmail.com>
2025-06-21 18:10:00 -0400

    Updated ThankYouPage.tsx

--- src/ThankYouPage.tsx
+++ src/ThankYouPage.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import React from 'react';
 import ThankYouPage from './components/ThankYouPage.tsx';
+import MetaPixel from './components/metaPixel';
 
 
 createRoot(document.getElementById('root')!).render(

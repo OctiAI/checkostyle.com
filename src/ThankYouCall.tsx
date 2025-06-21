17c2e04853031503330bb85b74adc94bae7eba24
OctiAI <project.thirdiris@gmail.com>
2025-06-21 18:10:35 -0400

    Updated ThankYouCall.tsx

--- src/ThankYouCall.tsx
+++ src/ThankYouCall.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import React from 'react';
 import ThankYouCall from './components/ThankYouCall.tsx';
+import MetaPixel from './components/metaPixel';
 
 
 createRoot(document.getElementById('root')!).render(

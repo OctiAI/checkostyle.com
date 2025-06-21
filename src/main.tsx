a90437af15c4cab93df1b99fe5dc242ddb0d3be5
OctiAI <project.thirdiris@gmail.com>
2025-06-21 18:12:18 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import MetaPixel from './components/metaPixel';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

add5788972e497fb5e8f86fd40f18e1b3f9344c0
OctiAI <project.thirdiris@gmail.com>
2025-06-21 18:09:23 -0400

    Updated form.tsx

--- src/form.tsx
+++ src/form.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import React from 'react';
 import Form from './components/Form';
+import MetaPixel from './components/metaPixel';
 
 
 createRoot(document.getElementById('root')!).render(

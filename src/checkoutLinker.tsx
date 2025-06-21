87eec2b1cf2e8ff2360d4d041a14591def851bad
OctiAI <project.thirdiris@gmail.com>
2025-06-21 18:09:43 -0400

    Updated checkoutLinker.tsx

--- src/checkoutLinker.tsx
+++ src/checkoutLinker.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import CheckoutPage from './components/checkout';
+import MetaPixel from './components/metaPixel';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

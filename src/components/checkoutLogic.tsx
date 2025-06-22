c3727d8357d7307c6eae0f0eb6a31d195e01a16f
OctiAI <project.thirdiris@gmail.com>
2025-06-21 21:40:35 -0400

    Updated checkoutLogic.tsx

--- src/components/checkoutLogic.tsx
+++ src/components/checkoutLogic.tsx
@@ -300,6 +300,7 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ planId, cart_fields }) => {
                 <div className="flex justify-between items-center">
                  <span>CheckoStyle DFY Setup Fee</span>;  
                   <span className="font-bold text-green-600">$499.00</span>
+                  <div className="flex justify-between items-center"></div>
                  <span>CheckoStyle Pro Plan</span>;  
                 <span className="font-bold text-green-600">$129.00</span>
               </div>)   
@@ -308,11 +309,9 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ planId, cart_fields }) => {
          }
         })()}
           
-   <div className="flex justify-between items-center">
-                 <span>CheckoStyle DFY Setup Fee</span>;  
-                  <span className="font-bold text-green-600">$499.00</span>
-                 <span>CheckoStyle Pro Plan</span>;  
-                <span className="font-bold text-green-600">$129.00</span>
+                <div className="flex justify-between items-center">
+                 <span>CheckoStyle Tester Plan</span>;  
+                <span className="font-bold text-green-600">$9.00</span>
               </div>
         </div>
       

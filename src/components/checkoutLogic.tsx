e25e74ceec8aceb82c70969ba22551398051c0ca
OctiAI <project.thirdiris@gmail.com>
2025-06-21 21:41:51 -0400

    Updated checkoutLogic.tsx

--- src/components/checkoutLogic.tsx
+++ src/components/checkoutLogic.tsx
@@ -303,13 +303,13 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({ planId, cart_fields }) => {
                 </div> <div className="flex justify-between items-center">
                <span>CheckoStyle Pro Plan</span>  
               <span className="font-bold text-green-600">$129.00</span>
-            </div>)   
+            </div>
+        </div>)   
             case 'CS_checko_enterprise_m':
               return <span>Book My Enterprise Call →</span>;
          }
         })()}
-            
-        </div>
+             
       
       {/* Submit Button */}
       <button

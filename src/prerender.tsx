a22be9de0e514d803b6f0163864cd90f12aabc3f
OctiAI <project.thirdiris@gmail.com>
2025-06-23 21:28:33 -0400

    Updated prerender.tsx

--- src/prerender.tsx
+++ src/prerender.tsx
@@ -4,8 +4,12 @@ import { renderToString } from "react-dom/server";
 import App from "./App";    // your root component
 import { vitePrerenderPlugin } from 'vite-prerender-plugin';  // ← import it here
 
-// This function is called once for each route you list below.
-export async function prerender() {
-  const html = renderToString(<App />);
-  return html;
+
+/**
+ * This default-exported function will be called
+ * once for each route you listed in vite.config.ts
+ */
+export default function prerender() {
+  // Simply render your <App /> to a string
+  return renderToString(<App />);
 }

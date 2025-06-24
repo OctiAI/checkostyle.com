37c0da721d5cef261b8a4c1c05a9f476cb8ac76f
OctiAI <project.thirdiris@gmail.com>
2025-06-23 21:16:53 -0400

    Updated prerender.tsx

--- src/prerender.tsx
+++ src/prerender.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import App from "./App";    // your root component
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';  // ← import it here
 
 // This function is called once for each route you list below.
 export async function prerender() {
